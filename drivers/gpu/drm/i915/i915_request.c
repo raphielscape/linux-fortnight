krasCGQ/linux

drm/i915: Avoid false -Wsometimes-uninitialized warning in busywait_stop
drivers/gpu/drm/i915/i915_request.c:1032:6: warning: variable 'this_cpu'
is used uninitialized whenever '&&' condition is false
[-Wsometimes-uninitialized]

Link: https://github.com/ClangBuiltLinux/linux/issues/415
Suggested-by: Nick Desaulniers <ndesaulniers@google.com>
Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
Signed-off-by: Albert I <kras@raphielgang.org>

drivers/gpu/drm/i915/i915_request.c
@@ -1311,8 +1311,9 @@ static unsigned long local_clock_us(unsigned int *cpu)
 static bool busywait_stop(unsigned long timeout, unsigned int cpu)
 {
 	unsigned int this_cpu;
+	unsigned long local_clock = local_clock_us(&this_cpu);
 
-	if (time_after(local_clock_us(&this_cpu), timeout))
+	if (time_after(local_clock, timeout))
 		return true;
 
 	return this_cpu != cpu;

raphielscape/linux-fortnight

Signed-off-by: Rapherion Rollerscaperers <rapherion@raphielgang.org>

drivers/gpu/drm/i915/i915_request.c
@@ -1483,8 +1483,9 @@ static unsigned long local_clock_ns(unsigned int *cpu)
 static bool busywait_stop(unsigned long timeout, unsigned int cpu)
 {
 	unsigned int this_cpu;
+	unsigned long local_clock = local_clock_ns(&this_cpu);
 
-	if (time_after(local_clock_ns(&this_cpu), timeout))
+	if (time_after(local_clock, timeout))
 		return true;
 
 	return this_cpu != cpu;